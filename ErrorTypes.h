7946c33d2ee7c5109d1da4ad03a91377e3f444e7
Leprechaun817 <aaronmgagern@sbcglobal.net>
2012-11-30 16:24:52 -0500

    Added copyright notices
    -Applied the notice to each file
    -Added COPYING.txt which contains the text of the license
    -This code is now licensed under GPLv3

--- ErrorTypes.h
+++ ErrorTypes.h
@@ -1,3 +1,27 @@
+/********************************************************************************************
+*********************************************************************************************
+Aaron's Tic-Tac-Toe Clone
+A 2 player verison of Tic-Tac-Toe game that's played on a console screen.
+
+Copyright (C) 2012 Aaron Gagern
+
+This file is part of Aaron's Tic-Tac-Toe Clone.
+
+Aaron's Tic-Tac-Toe Clone is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Aaron's Tic-Tac-Toe Clone is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Aaron's Tic-Tac-Toe Clone.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************************************
+********************************************************************************************/
+
 #pragma once
 
 struct ErrorTypes
